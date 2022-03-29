b65aae69869b42e296435d6d17c2b3c823048990
tokanada <davidt.3200@gmail.com>
2022-03-29 19:16:11 -0400

    Fixed upload SQL Code

--- admin_add_energyring.php
+++ admin_add_energyring.php
@@ -11,92 +11,57 @@ $quantity = $_POST['quantity'];
 $clone = $_POST['yesNo'];
 $spin_direction = $_POST['spinDirection'];
 $image = $_FILES["itemPicture"]["name"];
+$image_location = 'upload/'.$image;
 
 include 'secrets.php';
 
-// echo "<h1>";
-// echo $name;
-// echo "<br />";
-// echo $weight;
-// echo "<br />";
-// echo $attack;
-// echo "<br />";
-// echo $defense;
-// echo "<br />";
-// echo $stamina;
-// echo "<br />";
-// echo $generation;
-// echo "<br />";
-// echo $price;
-// echo "<br />";
-// echo $quantity;
-// echo "<br />";
-// echo $clone;
-// echo "<br />";
-// echo $spin_direction;
-// echo "<br />";
-// echo $image;
-// echo "<br />";
-// echo '</h1>';
-$image_location = 'upload/'.$image;
 
-echo "imagelocation set";
-$connection = mysqli_connect(Mydbserver, Mydbid, Mydbpassword, Mydatabase); 
-
-echo $connection;
-echo "<br />";
-
-$sql = "INSERT INTO EnergyRing (name, weight, attack, defense, stamina, quantity, generation, clone, spindirection, price, picture) VALUES ('" . $name . "','" . $weight . "','" . $attack ."','" . $defense . "','" . $stamina . "'," . strval($quantity) . "'," . $generation . "'," . $clone . "'," . $spin_direction . "'," . strval($price) . "'," . $image_location . ")" ;
-
-echo $sql;
-
-$result = mysqli_query($connection, $sql) or die(mysql_error($connection));  
+$connection = mysqli_connect(Mydbserver, Mydbid, Mydbpassword, Mydatabase);
 
 
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  exit();
+}
 
+$sql = "INSERT INTO EnergyRing (name, weight, attack, defense, stamina, quantity, generation, clone, spindirection, price, picture) VALUES ('" . $name . "','" . $weight . "','" . $attack . "','" . $defense . "','" . $stamina . "','" . $quantity . "','" . $generation . "','" . $clone . "','" . $spin_direction . "','" . $price . "','" . $image_location . "')" ;
+
+// Perform a query, check for error
+if (!mysqli_query($connection,$sql)) {
+  echo("Error description: " . mysqli_error($connection));
+}
 
 mysqli_close($connection);
-
-
-
 ?>
 
 <?php
 
-//echo "<br /><br /> For image: <br />";
-if ((($_FILES["imagefile"]["type"] == "image/png"))
-&& ($_FILES["imagefile"]["size"] < 2000000))
+if ($_FILES["itemPicture"]["type"] == "image/png")
   {
-  if ($_FILES["imagefile"]["error"] > 0)
+  if ($_FILES["itemPicture"]["error"] > 0)
     {
-    //echo "Return Code: " . $_FILES["imagefile"]["error"] . "<br />";
     }
   else
     {
-    //echo "Upload: " . $_FILES["imagefile"]["name"] . "<br />";
-    //echo "Type: " . $_FILES["imagefile"]["type"] . "<br />";
-    //echo "Size: " . ($_FILES["imagefile"]["size"] / 1024) . " Kb<br />";
-    //echo "Temp file: " . $_FILES["imagefile"]["tmp_name"] . "<br />";
 
-    if (file_exists("upload/" . $_FILES["imagefile"]["name"]))
+    if (file_exists("upload/" . $_FILES["itemPicture"]["name"]))
       {
-      //echo $_FILES["imagefile"]["name"] . " already exists. ";
+      echo $_FILES["itemPicture"]["name"] . " already exists. ";
       }
     else
       {
-      move_uploaded_file($_FILES["imagefile"]["tmp_name"],
-      "upload/" . $_FILES["imagefile"]["name"]);
-      //echo "Stored in: " . "upload/" . $_FILES["imagefile"]["name"];
+      move_uploaded_file($_FILES["itemPicture"]["tmp_name"],
+      "upload/" . $_FILES["itemPicture"]["name"]);
+      echo "Stored in: " . "upload/" . $_FILES["itemPicture"]["name"];
       }
     }
   }
 else
   {
-  //echo "Invalid image file";
   }
 ?>
 
 <?php
-  echo "<h1><a href='retrieve_books_2.php'>See available books.</a></h1>";
+echo "<h1><a href='retrieve_books_2.php'>See available books.</a></h1>";
 echo "<h1><a href='insert_new_book.html'>Insert more books?</a> </h1>";
 ?>
