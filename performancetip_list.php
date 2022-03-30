68d00ba2347de3aacffc5c1bbde35fe3d56d9437
TekNiBaLL <jexiao@bsu.edu>
2022-03-29 23:49:20 -0400

    update fusionwheel_list, performancetip_list and spintrack_list

---
+++ performancetip_list.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Performance Tip Product List</title>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+
+    <body>
+    <h1>Performance Tips</h1>
+    <table border="2" width="60%">
+        <thead>
+            <th></th>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Weight</th>
+            <th>Attack</th>
+            <th>Defense</th>
+            <th>Stamina</th>
+            <th>Generation</th>
+            <th>Price</th>
+        </thead>
+    <?php
+
+
+    include 'secrets.php';
+
+
+    $connection = mysqli_connect(Mydbserver, Mydbid, Mydbpassword, Mydatabase);
+
+    if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    exit();
+    }
+
+    $sql = "SELECT name, weight, attack, defense, stamina, generation, price, picture FROM FaceBolt";
+
+
+    $result = mysqli_query($connection, $sql) or die(mysql_error($connection));
+    $partType = "performancetip";
+
+
+    while ($resultArray = mysqli_fetch_array($result))
+    {
+        echo "<tr>";
+        echo "<td>";
+        echo "<a href='add_item_cart.php?id=" . $resultArray['name'] . "?type=" . $partType . "'>Add to Cart</a>";
+        echo "</td>";
+        echo "<td>";
+        echo "<img src=". $resultArray['picture']. ">";
+        echo "</td>";
+        echo "<td>";
+        echo $resultArray['name'];
+        echo "</td>";
+        echo "<td>";
+        echo $resultArray['weight'];
+        echo "</td>";
+        echo "<td>";
+        echo $resultArray['attack'];
+        echo "</td>";
+        echo "<td>";
+        echo $resultArray['defense'];
+        echo "</td>";
+        echo "<td>";
+        echo $resultArray['stamina'];
+        echo "</td>";
+        echo "<td>";
+        echo $resultArray['generation'];
+        echo "</td>";
+        echo "<td>";
+        echo "$" . $resultArray['price'];
+        echo "</td>";
+        echo "</tr>";
+    }
+
+    echo "</table>";
+    ?>
+
+</html>
